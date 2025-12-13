163d142b67ed2f33ab9e4cd905cdbb1582f15ccf
nickbhorton <horto202@umn.edu>
2025-12-13 06:10:14 -0700

    texting tex commands and script components

---
+++ src/mathjax-config.js
@@ -0,0 +1,14 @@
+window.MathJax = {
+    startup: {
+        ready() {
+        MathJax.startup.defaultReady();
+        const {STATE} = MathJax._.core.MathItem;
+        MathJax.tex2mml(String.raw`
+            \newcommand{\tv}[1]{\pmb{\mathscr{#1}}}
+            \renewcommand{\t}[1]{\mathscr{#1}}
+            \renewcommand{\v}[1]{\mathbf{#1}}
+            \newcommand{\uv}[1]{\hat{\mathbf{#1}}}
+        `);
+        }
+    }
+};
