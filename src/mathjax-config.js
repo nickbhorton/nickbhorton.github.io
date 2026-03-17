582b94469cf941c6ab9d5a30bb7c1e8bb9d0aefe
Nick Horton <nickbhorton40@gmail.com>
2026-03-17 06:16:31 -0600

    removing old stuff and adding first 11 equations from a general waveguide circuit theory

--- src/mathjax-config.js
+++ src/mathjax-config.js
@@ -7,10 +7,8 @@ window.MathJax = {
         MathJax.startup.defaultReady();
         const {STATE} = MathJax._.core.MathItem;
         MathJax.tex2mml(String.raw`
-            \newcommand{\tv}[1]{\pmb{\mathscr{#1}}}
-            \renewcommand{\t}[1]{\mathscr{#1}}
-            \renewcommand{\v}[1]{\mathbf{#1}}
-            \newcommand{\uv}[1]{\hat{\mathbf{#1}}}
+            \renewcommand{\v}[1]{\vec{#1}}
+            \newcommand{\uv}[1]{\hat{#1}}
         `);
         }
     }
